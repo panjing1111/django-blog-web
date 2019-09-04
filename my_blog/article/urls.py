16f3df9976a808076ea0f8ae5341287934093591
panjing <1106767857@qq.com>
2019-09-04 23:35:13 +0800

    commiter:panjing type: feat / fix / refactor / perf description:将部分函数视图改为类视图，提高代码复用性、可读性

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('user-article-list/<int:id>/', views.user_article_list, name='user_article_list'),
     path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
     path('article-create/', views.ArticleCreateView.as_view(), name='article_create'),
-    # path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
+    path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
     path('article-update/<int:id>/', views.article_update, name='article_update'),
 ]
