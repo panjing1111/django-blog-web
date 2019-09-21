5573f5271efc486df7b6430eff67dfcdf7ebaa50
panjing <1106767857@qq.com>
2019-09-21 13:47:49 +0800

    commiter:panjing type: feat / fix / refactor / perf description:

--- README.md
+++
@@ -1,5 +0,0 @@
-# django-blog-web
-使用django搭建个人博客网站
-
-1.基础部分复制的这里https://github.com/stacklens/django_blog_tutorial,
-实现了博客网站的基本功能
--- my_blog/article/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ArticlePost
-
-# Register your models here.
-
-
-# 注册ArticlePost到admin中
-admin.site.register(ArticlePost)
